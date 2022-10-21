8f62f2cf4bbb26fb51a4526bcb53380ae33d7ae5
shubhamsingla01 <s.singla1996@gmail.com>
2022-10-21 23:26:38 +0530

    Test v3 swagger to v2

--- swagger2locustio/strategy/base_strategy.py
+++ swagger2locustio/strategy/base_strategy.py
@@ -5,7 +5,8 @@ from typing import Set, Dict
 
 from swagger2locustio.parsers.base_parser import SwaggerBaseParser
 from swagger2locustio.parsers.swagger_v2 import SwaggerV2Parser
-from swagger2locustio.parsers.swagger_v3 import SwaggerV3Parser
+
+# from swagger2locustio.parsers.swagger_v3 import SwaggerV3Parser
 from swagger2locustio.generators.base_generator import BaseGenerator
 
 
@@ -27,10 +28,10 @@ class BaseStrategy:
         if not version:
             raise ValueError("No swagger version is specified")
         version = int(version[0])
-        if version == 2:
+        if version in (2, 3):
             parser: SwaggerBaseParser = SwaggerV2Parser()
-        elif version == 3:
-            parser = SwaggerV3Parser()
+        # elif version == 3:
+        #     parser = SwaggerV3Parser()
         else:
             raise ValueError("There is no support for %s version of swagger" % version)
         return parser
